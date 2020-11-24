11a957d498d76ced2ae10fade221a807e07d694b
frebucini <26899234+frebucini@users.noreply.github.com>
2020-11-24 16:59:54 +0100

    prova input enforcing mix

---
+++ code/experimental/asmeta.enforcer/src/org/asmeta/input_sanitisation_java/AirConditionerMain.java
@@ -0,0 +1,30 @@
+package org.asmeta.input_sanitisation_java;
+
+//Input: room temeperature (integer value)
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class AirConditionerMain {
+	
+	public static void main(String[] args) {	
+        int T =0;//default temperature value: 0
+        AirConditioner cond = new AirConditioner(0);	
+		
+        Scanner s = new Scanner(System.in); 
+        try {
+			while (T!=-1) { 
+				System.out.println("Conditioner ON: speed "+cond.getAirIntensity());			
+				System.out.println("Enter temperature value >");
+				T = s.nextInt(); //read room temperature
+				cond.setRoomTemperature(T);
+				cond.setAirIntensity();
+				}
+        }catch(InputMismatchException ex) {
+			System.out.println("Error, input illformed.");
+        }finally {
+	        s.close();
+	        System.out.println("Conditioner OFF");
+       }
+    }
+}
+
