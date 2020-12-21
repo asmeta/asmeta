a1e9e733876162cec566b96778b8147475082e28
scandurra <patrizia.scandurra@unibg.it>
2020-12-22 00:17:12 +0100

    Update ExtManagedSystem.java

--- code/experimental/asmeta.enforcer/src/org/asmeta/framework/managedSystem/ExtManagedSystem.java
+++ code/experimental/asmeta.enforcer/src/org/asmeta/framework/managedSystem/ExtManagedSystem.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 //import java.io.PrintWriter;
-//import java.net.Socket;
+
 
 import org.asmeta.framework.auxiliary.Utility;
 
