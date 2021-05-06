18f9a49987db3ac33a2ca7ccbc1392ae9b6c8d19
Angelo Gargantini <angelo.gargantini@unibg.it>
2021-05-06 10:43:39 +0200

    bug avalla on mac fixed

--- code/extensions/asmeta.validator/asmeta.validator.test/src/org/asmeta/avallaxt/validator/test/TestSingleFile.java
+++ code/extensions/asmeta.validator/asmeta.validator.test/src/org/asmeta/avallaxt/validator/test/TestSingleFile.java
@@ -11,20 +11,22 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.asmeta.parser.ParserResultLogger;
 import org.asmeta.xt.validator.AsmetaFromAvallaBuilder;
+import org.asmeta.xt.validator.AsmetaPrinterForAvalla;
 import org.asmeta.xt.validator.SimulatorWCov;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestSingleFile extends TestValidator {
 
 	@Test
 	public void testBuiler() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("scenariosfortest/lift.avalla", false, false);
 	}
 
 	@Test
 	public void testBuilerWithSpaces() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		//
 		Path model = Paths.get("scenariosfortest\\sub dir\\Lift.asm");
 		assert Files.exists(model);
@@ -49,32 +51,32 @@ public class TestSingleFile extends TestValidator {
 
 	@Test
 	public void testBuilerAbs() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("scenariosfortest/lift3.avalla", false, false);
 	}
 
 	@Test
 	public void testBuilerSameDir() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("scenariosfortest/lift4.avalla", false, false);
 	}
 
 	
 	@Test
 	public void testBuilerblock() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("scenariosfortest/block.avalla", false, false);
 	}
 
 	@Test
 	public void testBuilerExecblock() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("scenariosfortest/block2.avalla", false, false);
 	}
 
 	@Test
 	public void testBuilerExternalExecblock() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("scenariosfortest/block3.avalla", false, false);
 	}
 
@@ -82,13 +84,13 @@ public class TestSingleFile extends TestValidator {
 	
 	@Test
 	public void testCipher() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("example/fh/extend.avalla", false, false);
 	}
 	
 	@Test
 	public void testInvariant() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("scenariosfortest\\invariants\\scenario_inv.avalla", true, false);
 	}
 
@@ -96,36 +98,36 @@ public class TestSingleFile extends TestValidator {
 	
 	@Test
 	public void testSLE() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("..\\..\\..\\..\\asm_examples\\examples\\fsmsemantics\\Sle\\testEven1.avalla", false, false);
 	}
 	@Test
 	public void testATM1() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("scenariosforexamples\\atm\\atm1.avalla", false, false);
 	}
 
 	@Test
 	public void testModule1() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("scenariosfortest\\lift.avalla", false, false);
 	}
 
 	@Test
 	public void testModule2() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("D:\\AgHome\\Dropbox\\Documenti\\ricerca\\asm\\ABZ2020_casestudy\\Casestudy\\ASM model\\module\\myscenario.avalla", false, false);
 	}
 
 	@Test
 	public void testPillBox() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("D:\\AgHome\\Dropbox\\Documenti\\progetti\\quasmed_git\\PillboxASM\\onlyred_level2\\pillbox_2_scenario1.avalla", false, false);
 	}
 
 	@Test
 	public void testABZ2020CruiseCtrl() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		//Logger.getLogger(Simulator.class).setLevel(Level.ALL);
 		//test("C:\\Users\\garganti\\Dropbox\\Documenti\\ricerca\\asm\\ABZ2020_casestudy\\Casestudy\\ASM model\\scenarios\\CarSystem004scenario001.avalla", true);
 		test("D:\\AgHome\\Dropbox\\Documenti\\ricerca\\asm\\ABZ2020_casestudy\\Casestudy\\ASM model\\Car System\\scenarios\\CarSystem006scenario003.avalla",true, false);
@@ -133,7 +135,7 @@ public class TestSingleFile extends TestValidator {
 
 	@Test
 	public void testABZ2020_module1() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		//Logger.getLogger(Simulator.class).setLevel(Level.ALL);
 		//test("C:\\Users\\garganti\\Dropbox\\Documenti\\ricerca\\asm\\ABZ2020_casestudy\\Casestudy\\ASM model\\scenarios\\CarSystem004scenario001.avalla", true);
 		test("..\\..\\..\\..\\asm_examples\\examples\\ABZ2020\\CarSystemModule\\CarSystem001\\scenari\\HWExecutedRunning.avalla",false, false);
@@ -142,49 +144,50 @@ public class TestSingleFile extends TestValidator {
 	
 	@Test
 	public void testMon() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("scenariosfortest\\mon\\scenario.avalla", false, false);
 	}
 // with the use of import/modules
 	@Test
 	public void testMod1() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
+		
 		test("scenariosfortest\\withmodules\\scenario1.avalla", false, false);		
 	}
 	
 	@Test
 	public void testMod2() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
 		test("scenariosfortest\\withmodules\\scenarios\\scenario2.avalla", false, false);		
 	}
 	// import nested
 	@Test
 	public void testMod3() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
 		test("scenariosfortest\\withmodules\\scenario2i.avalla", false, false);		
 	}
 	
 	// import from a subdir
 	@Test
 	public void testMod4() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
 		test("scenariosfortest\\withmodules\\scenario3.avalla", false, false);		
 	}
 	
 	//Error: java.lang.IllegalArgumentException: 'other' has different root
 	@Test
 	public void testSafePillbox() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
 		test("D:\\GitHub\\ASMETA\\SafePillbox\\SafePillbox\\V04\\scenarios\\scenario3.avalla", false, false);		
 	}
 	
 	//Error: java.lang.IllegalArgumentException: 'other' has different root
 	@Test
 	public void testPillbox() throws Exception {
-		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.ALL);
-		Logger.getLogger("org.asmeta.parser").setLevel(Level.ALL);
 		test("D:\\AgHome\\progettidaSVNGIT\\asmeta\\asmeta\\asm_examples\\PillBox\\Level0\\pillbox_0_scenario1.avalla", false, false);		
 	}
 
+	@BeforeClass
+	static public void setuplogger() throws Exception {
+		Logger.getLogger(AsmetaFromAvallaBuilder.class).setLevel(Level.OFF);
+		Logger.getLogger(AsmetaPrinterForAvalla.class).setLevel(Level.ALL);		
+		Logger.getLogger("org.asmeta.parser").setLevel(Level.OFF);
+	}
+	
 	
 }
