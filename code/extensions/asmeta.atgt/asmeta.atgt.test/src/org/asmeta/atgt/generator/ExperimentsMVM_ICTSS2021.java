35d24fbbba9048e3592a11c22b8105ba61678891
Silvia Bonfanti <silvia.bonfanti@unibg.it>
2021-05-25 10:32:04 +0200

    update

--- code/extensions/asmeta.atgt/asmeta.atgt.test/src/org/asmeta/atgt/generator/ExperimentsMVM_ICTSS2021.java
+++ code/extensions/asmeta.atgt/asmeta.atgt.test/src/org/asmeta/atgt/generator/ExperimentsMVM_ICTSS2021.java
@@ -128,7 +128,7 @@ public class ExperimentsMVM_ICTSS2021 {
 
 	@Test
 	public void generateMVM1atatime() throws Exception {
-		String ex = "../../../../../mvm-asmeta/asm_models/VentilatoreASM_NewTime/Ventilatore4SimpleTimeLtdY.asm";
+		String ex = "../../../../../mvm-asmeta/asm_models/VentilatorICTSS/Ventilatore4SimpleTimeLtdY.asm";
 		// build all the criteria needed for the experiment
 		List<AsmCoverageBuilder> coverageCriteria = new ArrayList<>();
 		for (CriteriaEnum c : CriteriaEnum.values()) {
