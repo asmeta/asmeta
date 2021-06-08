37c76b599d467fe0f7321b48b4180f28bab8d453
Silvia Bonfanti <silvia.bonfanti@unibg.it>
2021-06-08 22:46:37 +0200

    new tests 07_06

--- code/extensions/asmeta.atgt/asmeta.atgt.test/src/org/asmeta/atgt/generator/ExperimentsMVM_ICTSS2021.java
+++ code/extensions/asmeta.atgt/asmeta.atgt.test/src/org/asmeta/atgt/generator/ExperimentsMVM_ICTSS2021.java
@@ -53,7 +53,7 @@ import atgt.testseqexport.toAvalla;
  */
 public class ExperimentsMVM_ICTSS2021 {
 
-	private static final int TIMEOUT_MS = 2400000; // 40 min, 7200000 - 2 hour, 3600000 - 1 hour , 2700000 -45 minutes 
+	private static final int TIMEOUT_MS = 600000; // 10 min, 2400000 - 40 min, 7200000 - 2 hour, 3600000 - 1 hour , 2700000 -45 minutes 
 
 	private static final String data = LocalDateTime.now().format(DateTimeFormatter.BASIC_ISO_DATE);
 	
