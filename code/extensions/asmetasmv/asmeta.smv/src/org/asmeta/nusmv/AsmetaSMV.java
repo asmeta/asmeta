3b3b954699f57a4d0a4aee0fa3e47df31aa3e51b
Angelo Gargantini <angelo.gargantini@unibg.it>
2021-06-30 10:57:52 +0200

    add quotes when path contains spaces

--- code/extensions/asmetasmv/asmeta.smv/src/org/asmeta/nusmv/AsmetaSMV.java
+++ code/extensions/asmetasmv/asmeta.smv/src/org/asmeta/nusmv/AsmetaSMV.java
@@ -219,6 +219,9 @@ public class AsmetaSMV {
 		StreamGobblerNuXmv sg = new StreamGobblerNuXmv(proc.getInputStream(), sb);
 		new Thread(sg).start();
 		TimeUnit.SECONDS.sleep(1);
+		// add quotes if needed
+		if (smvFileName.contains(" "))
+			smvFileName = "\""+ smvFileName + "\"";
 		// send some messages
 		System.out.println("read_model -i " + smvFileName + "\n");
 		bw.write("read_model -i " + smvFileName + "\n");
@@ -317,6 +320,9 @@ public class AsmetaSMV {
 				commands.add(Integer.toString(BMCLength));
 			}
 		}
+		// add quotes if needed
+		if (smvFileName.contains(" "))
+			smvFileName = "\""+ smvFileName + "\"";
 		commands.add(smvFileName);
 		if (false) {
 			String nusmvCommand = commands.subList(2, commands.size()).toString();
