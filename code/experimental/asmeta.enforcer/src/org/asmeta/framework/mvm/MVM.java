e8ecc7094e6a40b689610fc45adeb23a04aab44c
scandurra <patrizia.scandurra@unibg.it>
2021-10-19 14:19:15 +0200

    Update MVM.java

--- code/experimental/asmeta.enforcer/src/org/asmeta/framework/mvm/MVM.java
+++ code/experimental/asmeta.enforcer/src/org/asmeta/framework/mvm/MVM.java
@@ -1,12 +1,41 @@
+/**
+* 
+*
+* @author Patrizia Scandurra
+*/
 package org.asmeta.framework.mvm;
 
 import java.util.Map;
 
-public class MVM {
+import org.asmeta.framework.managedSystem.Effector;
+import org.asmeta.framework.managedSystem.ManagedSystem;
+import org.asmeta.framework.managedSystem.Probe;
+
+
+//Java class for the causal connection with the Arduino prototype of the MVM
+public class MVM extends ManagedSystem implements Probe, Effector{
 
 	public Map<String, String> getOutputForProbing() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public Probe getProbe() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Effector getEffector() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean shutDown() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
