c4b7969c00ac48b0fb0f05e0e603e1a557fb4691
Angelo Gargantini <angelo.gargantini@unibg.it>
2021-10-29 11:42:57 +0200

    a test case with any domain

---
+++ asm_examples/test/simulator/anydomain/AssignmentAny.asm
@@ -0,0 +1,22 @@
+// a simple example with an enum derived fron an AnyDomain
+// it fails - bug reported by MarioLilli oct 2021
+asm AssignmentAny
+import ../../../STDL/StandardLibrary
+
+signature:
+	// DOMAINS
+	domain MessageType subsetof Any
+	enum domain Message = {NAK | NNK | NK}
+	controlled protocolMessage: MessageType
+
+definitions:
+
+
+
+	// MAIN RULE
+	main rule r_Main =
+			protocolMessage:= NAK
+
+
+// INITIAL STATE
+default init s0:
--- code/core/asmeta.simulator/asmeta.simulator.test/testsrc/org/asmeta/simulator/TestAssignementConcretedomain.java
+++ code/core/asmeta.simulator/asmeta.simulator.test/testsrc/org/asmeta/simulator/TestAssignementConcretedomain.java
@@ -12,20 +12,25 @@ public class TestAssignementConcretedomain {
 	public void test() throws Exception {
 		Simulator sim = Util.getSimulatorForTestSpec("test/simulator/concreteAssgn.asm");
 		sim.run(1);
-		
 	}
 	@Test(expected = Exception.class)
 	public void testAny() throws Exception {
 		Simulator sim = Util.getSimulatorForTestSpec("test/simulator/concreteAssgnAny.asm");
 		sim.run(1);
-		
 	}
 
 	@Test(expected = Exception.class)
 	public void testUnion() throws Exception {
 		Simulator sim = Util.getSimulatorForTestSpec("test/simulator/concreteAssgnAnyunion.asm");
-		sim.run(1);
-		
+		sim.run(1);		
 	}
 
+	@Test
+	public void testAny2() throws Exception {
+		Simulator sim = Util.getSimulatorForTestSpec("test/simulator/anydomain/AssignmentAny.asm");
+		sim.run(1);		
+	}
+
+	
+	
 }
