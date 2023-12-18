04f7774da0e76b80765a316a97cbaef4f41f9b14
a-bombarda <andrea.bombarda@unibg.it>
2023-12-18 10:17:09 +0100

    fmv test runner

--- code/experimental/asmeta.fmvclib/src/asmeta/fmvc/testrunner/AsmetaFMVCTestRunner.java
+++ code/experimental/asmeta.fmvclib/src/asmeta/fmvc/testrunner/AsmetaFMVCTestRunner.java
@@ -3,34 +3,28 @@ package asmeta.fmvc.testrunner;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.Scanner;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
 import org.apache.commons.lang3.reflect.FieldUtils;
 
+import asmeta.fmvclib.annotations.AsmetaControlledLocation;
 import asmeta.fmvclib.annotations.AsmetaMonitoredLocation;
-import asmeta.fmvclib.controller.AsmetaFMVCController;
-import asmeta.fmvclib.model.AsmetaFMVCModel;
 import asmeta.fmvclib.view.AsmetaFMVCView;
 
 /**
  * The AsmetaFMVCTestRunner class
+ * 
+ * @author Andrea Bombarda
  */
 public class AsmetaFMVCTestRunner {
-
-	/**
-	 * The model
-	 */
-	AsmetaFMVCModel model;
 
 	/**
 	 * The view
 	 */
 	AsmetaFMVCView view;
-
-	/**
-	 * The controller
-	 */
-	AsmetaFMVCController controller;
 
 	/**
 	 * The path of the AVALLA scenario to be executed
@@ -40,91 +34,133 @@ public class AsmetaFMVCTestRunner {
 	/**
 	 * The constructor
 	 * 
-	 * @param model      the model
 	 * @param view       the view
-	 * @param controller the controller
 	 * @param scenario   the path of the scenario
 	 */
-	public AsmetaFMVCTestRunner(AsmetaFMVCModel model, AsmetaFMVCView view, AsmetaFMVCController controller,
-			String scenario) {
+	public AsmetaFMVCTestRunner(AsmetaFMVCView view, String scenario) {
 		super();
-		this.model = model;
 		this.view = view;
-		this.controller = controller;
 		this.scenario = scenario;
 	}
-	
-	/** 
+
+	/**
 	 * The method running the test from the avalla scenario
-	 * @throws IOException 
+	 * 
+	 * @throws IOException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
 	 */
-	public void runTest() throws IOException {
+	public void runTest() throws IOException, IllegalArgumentException, IllegalAccessException {
 		Scanner scanner = new Scanner(new File(this.scenario));
-		
+
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine();
-			
+
 			if (line.startsWith("//"))
 				// Comment, skip
 				continue;
-			
+
 			if (line.startsWith("scenario "))
 				// Initial line of the scenario, skip
 				continue;
-			
+
 			if (line.startsWith("load "))
 				// ASM loading instruction, skip
 				continue;
-			
+
 			if (line.startsWith("step"))
 				// Step instruction, skip
 				continue;
-			
+
 			if (line.startsWith("check "))
 				// Check instruction
 				runCheck(line.replace("check ", "").replace(";", ""));
-			
+
 			if (line.startsWith("set "))
 				// Set instruction
 				runSet(line.replace("set ", "").replace(";", ""));
 		}
-		
+
 		scanner.close();
-		
+
 	}
 
 	/**
 	 * Executes the check
 	 * 
 	 * @param line
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
 	 */
-	private void runCheck(String line) {
+	private void runCheck(String line) throws IllegalArgumentException, IllegalAccessException {
+		// Check execution
+		System.out.println("Executing check: " + line);
+		
 		// Extract the name and the value of the location
 		String locationName = line.split(" = ")[0];
 		String locationValue = line.split(" = ")[1];
-		
-		// TODO: Find the object annotated with the locationName 
-		List<Field> fieldListMonitoredLst = FieldUtils.getFieldsListWithAnnotation(view.getClass(),
-				AsmetaMonitoredLocation.class);
-		for (Field f : fieldListMonitoredLst) {
-			f.setAccessible(true);
-			
+
+		// Find the object annotated with the locationName
+		Field locationNameAnnotation = FieldUtils
+				.getFieldsListWithAnnotation(view.getClass(), AsmetaControlledLocation.class).stream()
+				.filter(x -> x.getAnnotation(AsmetaControlledLocation.class).asmLocationName()
+						.equals(locationName.split("\\(")[0]))
+				.findFirst().orElse(null);
+
+		// Check the value, depending on the object type
+		if (locationNameAnnotation != null) {
+			locationNameAnnotation.setAccessible(true);
+			Object obj = locationNameAnnotation.get(view);
+
+			try {
+				if (obj instanceof JTextField) {
+					((JTextField) obj).getText().equals(locationValue);
+				} else if (obj instanceof JLabel) {
+					assert ((JLabel) obj).getText().equals(locationValue);
+				} else if (obj instanceof JTable) {
+					// Since it is a table, the location name must contain the index
+					assert locationName.contains("(");
+					JTable table = ((JTable) obj);
+					
+					// ASMETA undef is Java empty string
+					if (locationValue.equals("undef"))
+						locationValue = "";
+					
+					// Extract the index
+					int index = Integer.parseInt(locationName.split("\\(")[1].split("\\)")[0]) - 1;
+					assert (locationValue.equals(table.getModel().getValueAt(index,  0)));
+				} else {
+					throw new RuntimeException("This type of component is not yet supported by the fMVC framework: "
+							+ obj.getClass());
+				}
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			}
 		}
-		
 	}
-	
+
 	/**
 	 * Executes the set
 	 * 
 	 * @param line
 	 */
 	private void runSet(String line) {
+		// Check execution
+		System.out.println("Executing set: " + line);
+		
 		// Extract the name and the value of the location
 		String locationName = line.split(" := ")[0];
 		String locationValue = line.split(" := ")[1];
 		
+		// Find the object annotated with the locationName
+		Field locationNameAnnotation = FieldUtils
+				.getFieldsListWithAnnotation(view.getClass(), AsmetaMonitoredLocation.class).stream()
+				.filter(x -> x.getAnnotation(AsmetaMonitoredLocation.class).asmLocationName()
+						.equals(locationName.split("\\(")[0]))
+				.findFirst().orElse(null);
 		
-		
+		System.out.println(locationNameAnnotation);
+
 	}
 
 }
