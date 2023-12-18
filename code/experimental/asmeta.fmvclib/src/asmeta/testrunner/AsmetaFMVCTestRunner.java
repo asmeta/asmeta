1dfb56c0cc7b58f975d756c7780fff81402462f0
Angelo Gargantini <angelo.gargantini@unibg.it>
2023-12-18 11:19:42 +0100

    first version

--- code/experimental/asmeta.fmvclib/src/asmeta/testrunner/AsmetaFMVCTestRunner.java
+++ code/experimental/asmeta.fmvclib/src/asmeta/testrunner/AsmetaFMVCTestRunner.java
@@ -1,4 +1,4 @@
-package testrunner;
+package asmeta.testrunner;
 
 import java.io.BufferedReader;
 import java.io.File;
---
+++ code/experimental/asmeta.fmvclib/src/org/asmeta/atgt/generator2/RandomMFReaderMemoryOneAction.java
@@ -0,0 +1,10 @@
+package org.asmeta.atgt.generator2;
+
+
+/** this class generates a radom value for each step but only for ONE function at the tile in a predefined set,
+ * while the others keep thery value
+ */
+
+public class RandomMFReaderMemoryOneAction extends RandomMFReaderMemory{
+
+}
