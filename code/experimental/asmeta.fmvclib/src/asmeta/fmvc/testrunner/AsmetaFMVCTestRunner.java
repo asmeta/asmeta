47ca34991a3461cf2e72cdf12e5d8c795459bcff
a-bombarda <andrea.bombarda@unibg.it>
2023-12-18 12:39:52 +0100

    Update AsmetaFMVCTestRunner.java

--- code/experimental/asmeta.fmvclib/src/asmeta/fmvc/testrunner/AsmetaFMVCTestRunner.java
+++ code/experimental/asmeta.fmvclib/src/asmeta/fmvc/testrunner/AsmetaFMVCTestRunner.java
@@ -3,15 +3,22 @@ package asmeta.fmvc.testrunner;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Scanner;
+
+import javax.swing.Timer;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 
 import org.apache.commons.lang3.reflect.FieldUtils;
-
 import asmeta.fmvclib.annotations.AsmetaControlledLocation;
 import asmeta.fmvclib.annotations.AsmetaMonitoredLocation;
+import asmeta.fmvclib.annotations.AsmetaMonitoredLocations;
+import asmeta.fmvclib.controller.ButtonColumn;
 import asmeta.fmvclib.view.AsmetaFMVCView;
 
 /**
@@ -34,8 +41,8 @@ public class AsmetaFMVCTestRunner {
 	/**
 	 * The constructor
 	 * 
-	 * @param view       the view
-	 * @param scenario   the path of the scenario
+	 * @param view     the view
+	 * @param scenario the path of the scenario
 	 */
 	public AsmetaFMVCTestRunner(AsmetaFMVCView view, String scenario) {
 		super();
@@ -82,7 +89,6 @@ public class AsmetaFMVCTestRunner {
 		}
 
 		scanner.close();
-
 	}
 
 	/**
@@ -95,7 +101,7 @@ public class AsmetaFMVCTestRunner {
 	private void runCheck(String line) throws IllegalArgumentException, IllegalAccessException {
 		// Check execution
 		System.out.println("Executing check: " + line);
-		
+
 		// Extract the name and the value of the location
 		String locationName = line.split(" = ")[0];
 		String locationValue = line.split(" = ")[1];
@@ -114,24 +120,24 @@ public class AsmetaFMVCTestRunner {
 
 			try {
 				if (obj instanceof JTextField) {
-					((JTextField) obj).getText().equals(locationValue);
+					assert ((JTextField) obj).getText().equals(locationValue);
 				} else if (obj instanceof JLabel) {
 					assert ((JLabel) obj).getText().equals(locationValue);
 				} else if (obj instanceof JTable) {
 					// Since it is a table, the location name must contain the index
 					assert locationName.contains("(");
 					JTable table = ((JTable) obj);
-					
+
 					// ASMETA undef is Java empty string
 					if (locationValue.equals("undef"))
 						locationValue = "";
-					
+
 					// Extract the index
 					int index = Integer.parseInt(locationName.split("\\(")[1].split("\\)")[0]) - 1;
-					assert (locationValue.equals(table.getModel().getValueAt(index,  0)));
+					assert (locationValue.equals(table.getModel().getValueAt(index, 0)));
 				} else {
-					throw new RuntimeException("This type of component is not yet supported by the fMVC framework: "
-							+ obj.getClass());
+					throw new RuntimeException(
+							"This type of component is not yet supported by the fMVC framework: " + obj.getClass());
 				}
 			} catch (IllegalArgumentException e) {
 				e.printStackTrace();
@@ -143,24 +149,85 @@ public class AsmetaFMVCTestRunner {
 	 * Executes the set
 	 * 
 	 * @param line
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
 	 */
-	private void runSet(String line) {
+	private void runSet(String line) throws IllegalArgumentException, IllegalAccessException {
 		// Check execution
 		System.out.println("Executing set: " + line);
-		
+
 		// Extract the name and the value of the location
 		String locationName = line.split(" := ")[0];
 		String locationValue = line.split(" := ")[1];
-		
+
 		// Find the object annotated with the locationName
 		Field locationNameAnnotation = FieldUtils
 				.getFieldsListWithAnnotation(view.getClass(), AsmetaMonitoredLocation.class).stream()
 				.filter(x -> x.getAnnotation(AsmetaMonitoredLocation.class).asmLocationName()
 						.equals(locationName.split("\\(")[0]))
 				.findFirst().orElse(null);
-		
-		System.out.println(locationNameAnnotation);
 
+		// If no location is found, it may be that the location has multiple annotations
+		if (locationNameAnnotation == null) {
+			List<Field> fieldList = FieldUtils.getFieldsListWithAnnotation(view.getClass(),
+					AsmetaMonitoredLocations.class);
+			for (Field f : fieldList) {
+				f.setAccessible(true);
+				for (AsmetaMonitoredLocation f1 : f.getAnnotation(AsmetaMonitoredLocations.class).value()) {
+					// TODO
+				}
+			}
+		} else {
+			// If a single annotation is found
+			locationNameAnnotation.setAccessible(true);
+			setObject(locationNameAnnotation.get(view), locationValue, locationName);
+		}
+	}
+
+	/**
+	 * Utility function used to set the value of an object
+	 * 
+	 * @param obj   the object to be updated
+	 * @param locationValue the value
+	 * @param locationName the name of the location
+	 */
+	private void setObject(Object obj, String locationValue, String locationName) {
+		try {
+			if (obj instanceof JTextField) {
+				((JTextField) obj).setText(locationValue);
+			} else if (obj instanceof JLabel) {
+				((JLabel) obj).setText(locationValue);
+			} else if (obj instanceof JSpinner) {
+				((JSpinner) obj).setValue(Integer.parseInt(locationValue));
+			} else if (obj instanceof JSlider) {
+				((JSlider) obj).setValue(Integer.parseInt(locationValue));
+			} else if (obj instanceof JButton) {
+				((JButton) obj).doClick();
+			} else if (obj instanceof Timer) {
+				if (((Timer) obj).getActionListeners().length > 0) {
+					((Timer) obj).getActionListeners()[0].actionPerformed(null);
+				}
+			} else if (obj instanceof ButtonColumn) {
+				// TODO: How to handle the ButtonColumn?
+			} else if (obj instanceof JTable) {
+				// Since it is a table, the location name must contain the index
+				assert locationName.contains("(");
+				JTable table = ((JTable) obj);
+
+				// ASMETA undef is Java empty string
+				if (locationValue.equals("undef"))
+					locationValue = "";
+
+				// Extract the index
+				int index = Integer.parseInt(locationName.split("\\(")[1].split("\\)")[0]) - 1;
+				table.getModel().setValueAt(locationValue, index, 0);
+			} else {
+				throw new RuntimeException(
+						"This type of component is not yet supported by the fMVC framework: " + obj.getClass());
+			}
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
