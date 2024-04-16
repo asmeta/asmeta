f4dc2c6562587292b480ac779e1d02f56e3b40f1
a-bombarda <andrea.bombarda@unibg.it>
2024-04-16 10:43:40 +0200

    Code refctoring

--- asmeta_models/tutorials/tutorial_FM24/pillbox_ground.asm
+++ asmeta_models/tutorials/tutorial_FM24/pillbox_ground.asm
@@ -26,8 +26,7 @@ signature:
 	// Functions checking the status of a Led
 	derived isOn: Drawer -> Boolean
 	derived isOff: Drawer -> Boolean
-	derived areOthersOn: Drawer -> Boolean
-	
+	derived areOthersOn: Drawer -> Boolean	
 	//*************************************************
 	// STATIC FUNCTIONS
 	//*************************************************
@@ -61,48 +60,38 @@ definitions:
 	rule r_choosePillToTake = choose $drawer in Drawer with 
 			isPillTobeTaken($drawer) and isOff($drawer) and not areOthersOn($drawer) do
 				// Rule to set the led red ON when the pill has to be taken
-				drawerLed($drawer) := ON 
-	
+				drawerLed($drawer) := ON 	
 	// Set the status for other drawers		
-	rule r_setOtherDrawers = forall $drawer in Drawer do
-		par		
-		if pillDeadlineHit($drawer) and isOff($drawer) then isPillTobeTaken($drawer) := true endif		
-		// Handle the evolution of the System when the LED is in ON state
-		if isOn($drawer) and
+	rule r_setOtherDrawers = forall $drawer in Drawer do par		
+			if pillDeadlineHit($drawer) and isOff($drawer) then isPillTobeTaken($drawer) := true endif		
+			// Handle the evolution of the System when the LED is in ON state:
 			// The pill has been taken, or the timer expires
-			isPillTaken($drawer) then r_reset[$drawer] endif
+			if isOn($drawer) and isPillTaken($drawer) then r_reset[$drawer] endif
 		endpar
 		
 	//*************************************************
 	// INVARIANTS AND TEMPORAL PROPERTIES
 	//*************************************************	
-
-	invariant inv_drawer1 over Drawer: 
-    (forall $d in Drawer with (isOn($d) implies not areOthersOn($d)))
-
-	// max one led is ON
-	// TODO
-	CTLSPEC ag(isOn(drawer1) implies (isOff(drawer2) and isOff(drawer3)))
+	invariant inv_drawer1 over Drawer: (forall $d in Drawer with (isOn($d) implies not areOthersOn($d)))
+	// Max one led is ON
 	CTLSPEC ag(isOn(drawer1) implies (not areOthersOn(drawer1)))
-	
+	CTLSPEC ag(isOn(drawer2) implies (not areOthersOn(drawer2)))
+	CTLSPEC ag(isOn(drawer3) implies (not areOthersOn(drawer3)))
+	// The following property describes with a forall the previous three
 	CTLSPEC ag((forall $d in Drawer with isOn($d) implies (not areOthersOn($d))))
-	
 	// If the patient takes the pill, red light will turn-off
 	CTLSPEC ag(pillDeadlineHit(drawer1) implies ef(isOn(drawer1)))
-	// with forall
+	CTLSPEC ag(pillDeadlineHit(drawer2) implies ef(isOn(drawer2)))
+	CTLSPEC ag(pillDeadlineHit(drawer3) implies ef(isOn(drawer3)))
+	// The following property describes with a forall the previous three
 	CTLSPEC (forall $d in Drawer with ag(pillDeadlineHit($d) implies ef(not areOthersOn($d))))
-
-	// these are false (it may happen that a new pill is never taken
-	CTLSPEC ag(pillDeadlineHit(drawer1) implies af(isOn(drawer1)))
-	LTLSPEC g(pillDeadlineHit(drawer1) implies f(isOn(drawer1)))
 	
 	//*************************************************
 	// MAIN Rule
 	//*************************************************	
 	main rule r_Main = par
 			// Non-determistic choice of the pill
-			r_choosePillToTake[]
-			
+			r_choosePillToTake[]			
 			// Handle other states
 			r_setOtherDrawers[]			
 		endpar
@@ -110,13 +99,11 @@ definitions:
 default init s0:	
 	// Turn-off all the led of the Drawers
 	function drawerLed($drawer in Drawer) = OFF
-
 	// Insert a drug in each drawer	
 	function drug($drawer in Drawer) = switch($drawer)
 			case drawer1 : TYLENOL
 			case drawer2 : ASPIRINE
 			case drawer3 : MOMENT
-		endswitch
-		
+		endswitch		
 	// Reset the status for the isPillTobeTaken function
 	function isPillTobeTaken($drawer in Drawer) = false
