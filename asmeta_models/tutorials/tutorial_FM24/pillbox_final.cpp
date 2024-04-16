1e8932c4335e9cc93e1f1b98af34c46daa7cb975
a-bombarda <andrea.bombarda@unibg.it>
2024-04-16 10:27:20 +0200

    C++ files

--- asmeta_models/tutorials/tutorial_FM24/pillbox_final.cpp
+++ asmeta_models/tutorials/tutorial_FM24/pillbox_final.cpp
@@ -1,84 +1,85 @@
-	/* pillbox_final.cpp automatically generated from ASM2CODE */
-	#include "pillbox_final.h"
-	
-	using namespace pillbox_finalnamespace;
-	
-	/* Conversion of ASM rules in C++ methods */
-	void pillbox_final::r_reset (Drawer* _drawer){
-		{ //par
-			drawerLed[1][_drawer] = OFF;
-			drugIndex[1][_drawer] = (drugIndex[0][_drawer] + 1);
-			isPillTobeTaken[1][_drawer] = false;
-		}//endpar
-	}
-	void pillbox_final::r_pillToBeTaken (Drawer* _drawer){
-		{ //par
-			if (! (isOn(_drawer))){ 
-				r_reset_timer(tenMinutes);
-			}
-			drawerLed[1][_drawer] = ON;
-		}//endpar
-	}
-	void pillbox_final::r_ON (Drawer* _drawer){
-		if (isOn(_drawer)){ 
-			if (expired(tenMinutes) | isPillTaken[_drawer]){ 
-				r_reset(_drawer);
-			}
+/* pillbox_final.cpp automatically generated from ASM2CODE */
+#include "pillbox_final.h"
+
+using namespace pillbox_finalnamespace;
+
+/* Conversion of ASM rules in C++ methods */
+void pillbox_final::r_reset (Drawer* _drawer){
+	{ //par
+		drawerLed[1][_drawer] = OFF;
+		drugIndex[1][_drawer] = (drugIndex[0][_drawer] + 1);
+		isPillTobeTaken[1][_drawer] = false;
+	}//endpar
+}
+void pillbox_final::r_pillToBeTaken (Drawer* _drawer){
+	{ //par
+		if (! (isOn(_drawer))){ 
+			r_reset_timer(tenMinutes);
+		}
+		drawerLed[1][_drawer] = ON;
+	}//endpar
+}
+void pillbox_final::r_ON (Drawer* _drawer){
+	if (isOn(_drawer)){ 
+		if (expired(tenMinutes) | isPillTaken[_drawer]){ 
+			r_reset(_drawer);
 		}
 	}
-	void pillbox_final::r_choosePillToTake(){
-		vector<const Drawer*> point0;
-		for(const auto& _drawer : Drawer::elems)
-		if(isPillTobeTaken[0][_drawer] & isOff(_drawer) & ! (areOthersOn(_drawer))){
-		point0.push_back(&(*_drawer));
-		}
-		int rndm = rand() % point0.size();
-		{
-		auto _drawer = *point0[rndm];
-		  if(point0.size()>0){
-			r_pillToBeTaken(&_drawer);
-			 }
-		}
+}
+void pillbox_final::r_choosePillToTake(){
+	vector<const Drawer*> point0;
+	for(const auto& _drawer : Drawer::elems)
+	if(isPillTobeTaken[0][_drawer] & isOff(_drawer) & ! (areOthersOn(_drawer))){
+	point0.push_back(&(*_drawer));
 	}
-	void pillbox_final::r_setOtherDrawers(){
-		for(auto _drawer : Drawer::elems)
-			if(true){	
-				{ //par
-					if (pillDeadlineHit(_drawer)){ 
-						isPillTobeTaken[1][_drawer] = true;
-					}
-					if (isThereAnyOtherDeadline(_drawer)){ 
-						r_ON(_drawer);
-					}
-				}//endpar
-			}
-	}
-	void pillbox_final::r_Main(){
-		{ //par
-			r_choosePillToTake();
-			r_setOtherDrawers();
-		}//endpar
-	}
-	
-	/* Static function definition */
-	bool pillbox_final::isOn(Drawer* _d){return (drawerLed[0][_d] == ON);}
-	bool pillbox_final::isOff(Drawer* _d){return (drawerLed[0][_d] == OFF);}
-	bool pillbox_final::areOthersOn(Drawer* _d){return [&](){      /*<--- caseTerm*/ 
-		if(_d==drawer1) 
-			return isOn(drawer2) | isOn(drawer3);
-		else if(_d==drawer2)
-			return isOn(drawer1) | isOn(drawer3);
-		else if(_d==drawer3)
-			return isOn(drawer2) | isOn(drawer1);
-	   }();}
-	bool pillbox_final::pillDeadlineHit(Drawer* _d){return (time_consumption[0][_d][drugIndex[0][_d]] <= mCurrTimeSecs);}
-	bool pillbox_final::isThereAnyOtherDeadline(Drawer* _d){return (drugIndex[0][_d] < (unsigned int)((time_consumption[0][_d]).size()));}
-	/* Function and domain initialization */
-	pillbox_final::pillbox_final(): 
-	//Static domain initialization 
-	LedLights_elems({OFF,ON}), 
-	Drugs_elems({TYLENOL,ASPIRINE,MOMENT})
+	int rndm = rand() % point0.size();
 	{
+	auto _drawer = *point0[rndm];
+	  if(point0.size()>0){
+		r_pillToBeTaken(&_drawer);
+		 }
+	}
+}
+void pillbox_final::r_setOtherDrawers(){
+	for(auto _drawer : Drawer::elems)
+		if(true){	
+			{ //par
+				if (pillDeadlineHit(_drawer)){ 
+					isPillTobeTaken[1][_drawer] = true;
+				}
+				if (isThereAnyOtherDeadline(_drawer)){ 
+					r_ON(_drawer);
+				}
+			}//endpar
+		}
+}
+void pillbox_final::r_Main(){
+	{ //par
+		r_choosePillToTake();
+		r_setOtherDrawers();
+	}//endpar
+}
+
+/* Static function definition */
+bool pillbox_final::isOn(Drawer* _d){return (drawerLed[0][_d] == ON);}
+bool pillbox_final::isOff(Drawer* _d){return (drawerLed[0][_d] == OFF);}
+bool pillbox_final::areOthersOn(Drawer* _d){return [&](){      /*<--- caseTerm*/ 
+	if(_d==drawer1) 
+		return isOn(drawer2) | isOn(drawer3);
+	else if(_d==drawer2)
+		return isOn(drawer1) | isOn(drawer3);
+	else if(_d==drawer3)
+		return isOn(drawer2) | isOn(drawer1);
+   }();}
+bool pillbox_final::pillDeadlineHit(Drawer* _d){return (time_consumption[0][_d][drugIndex[0][_d]] <= mCurrTimeSecs);}
+bool pillbox_final::isThereAnyOtherDeadline(Drawer* _d){return (drugIndex[0][_d] < (unsigned int)((time_consumption[0][_d]).size()));}
+
+/* Function and domain initialization */
+pillbox_final::pillbox_final(): 
+//Static domain initialization 
+LedLights_elems({OFF,ON}), 
+Drugs_elems({TYLENOL,ASPIRINE,MOMENT})
+{
 	/* Init static functions Abstract domain */
 	tenMinutes = new Timer;
 	drawer1 = new Drawer;
@@ -86,8 +87,8 @@
 	drawer3 = new Drawer;
 	/* Function initialization */
 	for(const auto& _drawer : Drawer::elems){
-	drawerLed[0].insert({_drawer,OFF});
-	drawerLed[1].insert({_drawer,OFF});
+		drawerLed[0].insert({_drawer,OFF});
+		drawerLed[1].insert({_drawer,OFF});
 	}
 	for(const auto& _drawer : Drawer::elems){
 	time_consumption[0].insert({_drawer,[&](){      /*<--- caseTerm*/ 
@@ -108,7 +109,7 @@
 	   }()});
 	}
 	for(const auto& _drawer : Drawer::elems){
-	drug[0].insert({_drawer,[&](){      /*<--- caseTerm*/ 
+	drug[0].insert({_drawer,[&](){      			/*<--- caseTerm*/ 
 		if(_drawer==drawer1) 
 			return TYLENOL;
 		else if(_drawer==drawer2)
@@ -116,7 +117,7 @@
 		else if(_drawer==drawer3)
 			return MOMENT;
 	   }()});
-	drug[1].insert({_drawer,[&](){      /*<--- caseTerm*/ 
+	drug[1].insert({_drawer,[&](){      			/*<--- caseTerm*/ 
 		if(_drawer==drawer1) 
 			return TYLENOL;
 		else if(_drawer==drawer2)
@@ -126,40 +127,38 @@
 	   }()});
 	}
 	for(const auto& _drawer : Drawer::elems){
-	drugIndex[0].insert({_drawer,0});
-	drugIndex[1].insert({_drawer,0});
+		drugIndex[0].insert({_drawer,0});
+		drugIndex[1].insert({_drawer,0});
 	}
 	for(const auto& _t : Timer::elems){
-	duration[0].insert({_t,600});
-	duration[1].insert({_t,600});
+		duration[0].insert({_t,600});
+		duration[1].insert({_t,600});
 	}
 	for(const auto& _t : Timer::elems){
-	start[0].insert({_t,currentTime(_t)});
-	start[1].insert({_t,currentTime(_t)});
+		start[0].insert({_t,currentTime(_t)});
+		start[1].insert({_t,currentTime(_t)});
 	}
-	}
+}
 
-	
-	/* initialize controlled functions that contains monitored functions in the init term */
-	void pillbox_final::initControlledWithMonitored(){
-	}
-	
+/* initialize controlled functions that contains monitored functions in the init term */
+void pillbox_final::initControlledWithMonitored(){
+}
 
-	/* Apply the update set */
-	void pillbox_final::fireUpdateSet(){
-		drawerLed[0] = drawerLed[1];
-		time_consumption[0] = time_consumption[1];
-		drug[0] = drug[1];
-		drugIndex[0] = drugIndex[1];
-		isPillTobeTaken[0] = isPillTobeTaken[1];
-		start[0] = start[1];
-		duration[0] = duration[1];
-	}
-	
-	/* init static functions and elements of abstract domains */
-	set< Drawer*>Drawer::elems;
-	Timer*pillbox_final::tenMinutes;
-	Drawer*pillbox_final::drawer1;
-	Drawer*pillbox_final::drawer2;
-	Drawer*pillbox_final::drawer3;
-	
+/* Apply the update set */
+void pillbox_final::fireUpdateSet(){
+	drawerLed[0] = drawerLed[1];
+	time_consumption[0] = time_consumption[1];
+	drug[0] = drug[1];
+	drugIndex[0] = drugIndex[1];
+	isPillTobeTaken[0] = isPillTobeTaken[1];
+	start[0] = start[1];
+	duration[0] = duration[1];
+}
+
+/* init static functions and elements of abstract domains */
+set< Drawer*>Drawer::elems;
+Timer*pillbox_final::tenMinutes;
+Drawer*pillbox_final::drawer1;
+Drawer*pillbox_final::drawer2;
+Drawer*pillbox_final::drawer3;
+
--- asmeta_models/tutorials/tutorial_FM24/pillbox_final.h
+++ asmeta_models/tutorials/tutorial_FM24/pillbox_final.h
@@ -1,57 +1,41 @@
-/* this file is under copyright*/
-	// pillbox_final.h automatically generated from ASMETA2CODE
-	#ifndef PILLBOX_FINAL_H
-	#define PILLBOX_FINAL_H
-	
-	/*Arduino.h uses WString instead... */
-	#include <string.h>				
-	#include <iostream> 
-	#include <vector> 
-	#include <set>
-	#include <map>
-	#include <list>
-	//Andrea Belotti
-	#include <chrono>
-	//#include <tuple>
-	//#include <bits/stl_tree.h>
-	
-	using namespace std;
-	
-	//typedef std::string String;
-	#define ANY String
-	
-	
-	#include "../../STDL/TimeLibrarySimple.h" 
+// pillbox_final.h automatically generated from ASMETA2CODE
+#ifndef PILLBOX_FINAL_H
+#define PILLBOX_FINAL_H
+
+#include <string.h>				
+#include <iostream> 
+#include <vector> 
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+
+using namespace std;
+
+#define ANY String
+#include "../../STDL/TimeLibrarySimple.h" 
 	
 	
 /* DOMAIN DEFINITIONS */
 namespace pillbox_finalnamespace{
 	class Drawer;
 	enum LedLights {OFF, ON};
-	
 	enum Drugs {TYLENOL, ASPIRINE, MOMENT};
+}
 	
-	 //devo togliere questo in Timer perché non serve
-	}
+using namespace pillbox_finalnamespace;
 
-	
-	using namespace pillbox_finalnamespace;
-	
-	class pillbox_finalnamespace::Drawer{
-	public:
+class pillbox_finalnamespace::Drawer{
+public:
 	static set<Drawer*> elems;
 	Drawer(){elems.insert(this);}
-	};
-	 //devo togliere questo in Timer perché non serve
-	
-	class pillbox_final : public virtual TimeLibrarySimple{
-	  
+};
+
+class pillbox_final : public virtual TimeLibrarySimple{  
 	/* DOMAIN CONTAINERS */
 	const set<LedLights> LedLights_elems;
-	
 	const set<Drugs> Drugs_elems;
-	
-	public:
+public:
 	/* FUNCTIONS */
 	map<Drawer*, bool> isPillTaken;
 	map<Drawer*, LedLights> drawerLed[2];
@@ -60,18 +44,15 @@ namespace pillbox_finalnamespace{
 	map<Drawer*, unsigned int> drugIndex[2];
 	map<Drawer*, bool> isPillTobeTaken[2];
 	static Timer* tenMinutes;
-	
 	bool isOn (Drawer* param0_isOn);
 	bool isOff (Drawer* param0_isOff);
 	bool areOthersOn (Drawer* param0_areOthersOn);
 	bool pillDeadlineHit (Drawer* param0_pillDeadlineHit);
 	bool isThereAnyOtherDeadline (Drawer* param0_isThereAnyOtherDeadline);
 	static Drawer* drawer1;
-	
 	static Drawer* drawer2;
-	
 	static Drawer* drawer3;
-	
+
 	/* RULE DEFINITION */
 	void r_reset (Drawer* _drawer);
 	void r_pillToBeTaken (Drawer* _drawer);
@@ -81,14 +62,9 @@ namespace pillbox_finalnamespace{
 	void r_Main();
 	
 	pillbox_final();
-	
 	void initControlledWithMonitored();
-	
 	void getInputs();
-	
 	void setOutputs(); 
-	
 	void fireUpdateSet();
-
-	};
-	#endif
+};
+#endif
