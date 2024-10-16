b580f7c29426ff330d42b601a12fd26f7c9f2a72
Angelo Gargantini <angelo.gargantini@unibg.it>
2024-10-16 13:48:50 +0200

    firt commit

---
+++ code/experimental/asmeta.evotest/asmeta.evotest.experiments/src/asmeta/evotest/experiments/TestExperimemts.java
@@ -0,0 +1,13 @@
+package asmeta.evotest.experiments;
+
+import org.asmeta.atgt.generator2.AsmTestGeneratorBySimulation;
+
+public class TestExperimemts {
+	
+	
+	
+	public static void main(String[] args) {
+		AsmTestGeneratorBySimulation  gen = new AsmTestGeneratorBySimulation(null, 10,10);
+	}
+
+}
