21fcf07e7334aaeb589962d9d7f0f9f51adb1d31
isaacmaffeis <isaacmaffeis@gmail.com>
2024-10-17 23:02:08 +0200

    creating terms and IF
    Rule and RulesImpl terms and the two interface Rules and RulesAdder
    -> objects with the aim of cover the rules

---
+++ code/experimental/asmetal2java/asmetal2java_codegen/src/org/asmeta/asm2java/evosuite/RulesImpl.java
@@ -0,0 +1,62 @@
+package org.asmeta.asm2java.evosuite;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * The {@code RulesImpl} class is a concrete implementation of both the {@code Rules} and {@code RulesAdder} interfaces.
+ */
+public final class RulesImpl implements Rules, RulesAdder {
+
+	/** rules map {name:Rule} */
+    private Map<String, Rule> rules;
+
+    /**
+     * Constructs an empty {@code RulesImpl} instance with an empty rule collection.
+     */
+    public RulesImpl() {
+        this.rules = new HashMap<>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addRule(String name, Rule rule) throws IllegalArgumentException {
+        if (rules.containsKey(name)) {
+            throw new IllegalArgumentException("Rule with the same name already exists: " + name);
+        }
+        rules.put(name, rule);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isEmpty() {
+        return rules.isEmpty();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Set<String> getRulesName() {
+        return rules.keySet();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> getRuleBranches(String name) {
+        if (rules.containsKey(name)) {
+            return rules.get(name).getBranches();
+        }
+        return null;
+    }
+
+}
+
