8419cc07109df59af070b902adbf1940013dba66
isaacmaffeis <isaacmaffeis@gmail.com>
2024-11-02 20:33:13 +0100

    updated the main

--- code/experimental/asmeta.evotest/evoAsmetaTG/src/main/java/asmeta/asmeta/evotest/App.java
+++ code/experimental/asmeta.evotest/evoAsmetaTG/src/main/java/asmeta/asmeta/evotest/App.java
@@ -6,20 +6,35 @@ import java.util.List;
 
 import org.asmeta.asm2java.main.Asmeta2JavaCLI;
 
+import asmeta.junit2avalla.main.Junit2AvallaCLI;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) throws IOException {
-    	/*
+    	
+    	String name = "\"input/RegistroDiCassav4.asm\"";
+    	
     	// translate to java
-    	Asmeta2JavaCLI.main(new String[]{"ex.asm"});
+    	Asmeta2JavaCLI.main(new String[]{"-input",name,"-output","evosuite-target","-mode","testGen","-Dcompiler=true"});
+    	
 		// executing evouite
-    	List<String> commands = Arrays.asList("java8", "-jar", "evosuite.jar");
-    	ProcessBuilder pb = new ProcessBuilder(commands);
-    	pb.start();
-    	// traduci da Junit ad avalla
-    	// TODO
-    	 */
+        List<String> commands = Arrays.asList("cmd.exe", "/c", "java8", "-jar", "evosuite-1.0.6.jar", "-target", "evosuite-target", "-class", "RegistroDiCassav4_ATG",
+        		"-criterion","LINE:BRANCH","-Dminimize=true","-Dassertion_strategy=all");
+
+        ProcessBuilder pb = new ProcessBuilder(commands);
+        pb.inheritIO();  // per mostrare l'output nel console corrente
+        try {
+            Process process = pb.start();
+            process.waitFor();  // Aspetta il completamento del processo
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        // translate to avalla
+        Junit2AvallaCLI.main(new String[] {"-input","evosuite-tests/RegistroDiCassav4_ATG_ESTest.java"});
+
+    	 
     }
 }
