1676d7ff999da1640c37ad0c69b64386010eec0b
Angelo Gargantini <angelo.gargantini@unibg.it>
2025-03-28 23:58:42 +0100

    fixed incosisten updates

--- code/extensions/asmetasmv/asmeta.smv.test/test_src/org/asmeta/nusmv/main/AsmetaSMVtestTranslate.java
+++ code/extensions/asmetasmv/asmeta.smv.test/test_src/org/asmeta/nusmv/main/AsmetaSMVtestTranslate.java
@@ -411,10 +411,11 @@ public class AsmetaSMVtestTranslate {
 
 	@Test
 	public void testAmanUndef() throws IOException {
-		// it is worng when tralsating undef .. why itis using undef and not UNDEF????
+		Path smv_fileName = Path.of("examples/aman.smv");		
+		Files.deleteIfExists(smv_fileName);
+		// it is wrong when translating undef .. why it is using "undef" and not UNDEF????
 		testOneSpec("examples/aman.asm");
-		Path fileName = Path.of("examples/aman.smv");
-		String str = Files.readString(fileName);
+		String str = Files.readString(smv_fileName);
 		System.out.println(str);
 		//it should not translate this as undef
 		assertFalse(str.contains("m_airplane = undef"));
