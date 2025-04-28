b95df8dfee88e2167ab2a43791e00f83453203aa
nicopellegrinelli <nico.pellenp@gmail.com>
2025-04-28 16:40:29 +0200

    Add spec to experiments and fill features_vs_specifications.xlsx

---
+++ code/experimental/asmeta.evotest/asmeta.evotest.experiments/src/main/resources/models/GuessBigNumber.asm
@@ -0,0 +1,46 @@
+/* A simple game in which the player has to guess a hidden number.
+ * The player has 10 guesses at his disposal and on each attempt the game indicates
+ * whether the number entered is higher or lower than the hidden number.
+ */
+
+asm GuessBigNumber
+
+import STDL/StandardLibrary
+import MessagePrinter
+
+signature:
+	// DOMAINS
+	domain Number subsetof Integer
+	domain Attempt subsetof Integer
+	monitored guess: Number
+	static hidden: Number
+	controlled attempt: Attempt
+	controlled win: Boolean
+
+definitions:
+	// DOMAIN DEFINITIONS
+	domain Number = {0 : 10000}
+	domain Attempt = {0 : 10}
+	
+	function hidden = 7524
+
+	// MAIN RULE
+	main rule r_Main =
+		if attempt > 0 then
+			if guess = hidden then
+				par
+					attempt := 0
+					win := true
+				endpar
+			else
+				par
+					r_printGuess[guess > hidden]
+					attempt := attempt - 1
+				endpar
+			endif
+		endif
+
+// INITIAL STATE
+default init s0:
+	function attempt = 10
+	function win = false
