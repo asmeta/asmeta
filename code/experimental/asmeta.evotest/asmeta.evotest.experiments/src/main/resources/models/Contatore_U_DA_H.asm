4ebfad673b941b0dae7e872e5b0512f591b03d60
nicopellegrinelli <nico.pellenp@gmail.com>
2025-05-12 17:09:43 +0200

    Change keywords with non-keywords

--- code/experimental/asmeta.evotest/asmeta.evotest.experiments/src/main/resources/models/Contatore_U_DA_H.asm
+++ code/experimental/asmeta.evotest/asmeta.evotest.experiments/src/main/resources/models/Contatore_U_DA_H.asm
@@ -1,6 +1,6 @@
 // LUZZANA
 
-asm Contatore_U_DA_H  //Nome della macchina a stati astratti
+asm Contatore_U_DA_H //Nome della macchina a stati astratti
 
 import STDL/StandardLibrary  //import della libreria standard
 
@@ -16,7 +16,7 @@ signature:  //definizione del vocabolario
 	dynamic monitored click : Boolean  
 	dynamic controlled unit : Unita
 	dynamic controlled da : Decine    
-	dynamic controlled h : Centinaia  
+	dynamic controlled c : Centinaia
 	dynamic controlled m : Migliaia
   
 /*Definizioni di domini*/
@@ -29,23 +29,23 @@ definitions:
 /*Definizione delle regole*/
 //regola che incrementa le migliaia
 	rule r_upMigliaia =
-		m := (m+ 1) mod 10
+		m := (m + 1) mod 10
 
 
 //Regola che incrementa le Centinaia, modulo 10 per resettare il valore
 	macro rule r_upCentinaia = 
 		par
-			if h=9 then
+			if c = 9 then
 				r_upMigliaia[]
 			endif
-			h := (h+ 1) mod 10
+			c := (c + 1) mod 10
 		endpar
 
 
 //regola che incrementa le Decine, modulo 10 per resettare il valore
 	macro rule r_upDecine =  
 		par
-			if da=9 then
+			if da = 9 then
 				r_upCentinaia[]
 			endif
 			da := (da + 1) mod 10
@@ -55,10 +55,10 @@ definitions:
 //regola che incrementa le unit�, modulo 10 per resettare il valore
 	macro rule r_upUnita =
 		par
-			if unit=9 then
+			if unit = 9 then
 				r_upDecine[] 
 			endif 
-			unit := (unit+1) mod 10 
+			unit := (unit + 1) mod 10
 		endpar 
 
 /*Definizione della main rule*/	
@@ -74,5 +74,5 @@ default init s1:
 	function click = false    
 	function unit = 0     //I contatori partono da zero
 	function da = 0     
-	function h = 0
+	function c = 0
 	function m = 0
--- code/experimental/asmeta.evotest/asmeta.evotest.experiments/src/main/resources/models/TrafficLightv2.asm
+++ code/experimental/asmeta.evotest/asmeta.evotest.experiments/src/main/resources/models/TrafficLightv2.asm
@@ -3,12 +3,12 @@ asm TrafficLightv2
 import STDL/StandardLibrary
 
 /*
- * The model includes four states, red, green, yellow, and pending, and an internal variable count.
+ * The model includes four states, red, green, yellow, and pending, and an internal variable timer.
  * The time is explicit, i.e., each transition consumes a unit of time (seconds for instance). 
- * The system starts in red and waits 60s seconds checking the variable count and then moves to green. 
+ * The system starts in red and waits 60s seconds checking the variable timer and then moves to green.
  * Then, the system waits for pedestrians.
- * When a pedestrian arrives, if the count is less than 60s, the system goes to pending,
- * letting the pedestrian wait for the count to reach 60.
+ * When a pedestrian arrives, if the timer is less than 60s, the system goes to pending,
+ * letting the pedestrian wait for the timer to reach 60.
  * Instead, if the traffic light was green for at least 60s, the light becomes yellow for 5 seconds before going to red.
  * The EFSM has also outputs that correspond to light colours.
  */
@@ -19,7 +19,7 @@ signature:
 	enum domain States = {RED | GREEN | YELLOW | PENDING}
 	domain TimerDomain subsetof Integer
 	// FUNCTIONS
-	controlled count: TimerDomain
+	controlled timer: TimerDomain
 	controlled status: States
 	
 	monitored pedestrian: Boolean
@@ -33,54 +33,54 @@ definitions:
 	domain TimerDomain = {0 : 6}
 
 	// FUNCTION DEFINITIONS
-	function timeOut = (count >= 6)
-	function yellow_timeOut = (count >= 1)
-	function interupt = (pedestrian = true and count < 6)
+	function timeOut = (timer >= 6)
+	function yellow_timeOut = (timer >= 1)
+	function interupt = (pedestrian = true and timer < 6)
 
 	// RULE DEFINITIONS
 	rule r_red = 
 		if(timeOut) then 
 			par
 				status := GREEN
-				count := 0
+				timer := 0
 			endpar
 		else
-			count := count + 1
+			timer := timer + 1
 		endif
 
 	rule r_green = 
 		if(interupt) then
 			par
 				status := PENDING
-				count := 0
+				timer := 0
 			endpar
 		else if(timeOut) then 
 			par
 				status := YELLOW
-				count := 0
+				timer := 0
 			endpar
 		else
-			count := count + 1
+			timer := timer + 1
 		endif endif
 	
 	rule r_yellow = 
 		if(yellow_timeOut) then 
 			par
 				status := RED
-				count := 0
+				timer := 0
 			endpar
 		else
-			count := count + 1
+			timer := timer + 1
 		endif
 		
 	rule r_pending = 
 		if(timeOut) then 
 			par
 				status := RED
-				count := 0
+				timer := 0
 			endpar
 		else
-			count := count + 1
+			timer := timer + 1
 		endif
 	
 	// INVARIANTS
@@ -101,4 +101,4 @@ definitions:
 // INITIAL STATE
 default init s0:
 	function status = RED
-	function count = 0
+	function timer = 0
