ed2e6fb10687129f8b2af99bbeca470a50de521f
silviabonfanti <silvia.bonfanti@unibg.it>
2025-10-24 16:13:54 +0200

    Update MorraCineseProperties.asm

--- asmeta_models/models/MorraCinese/MorraCineseProperties.asm
+++ asmeta_models/models/MorraCinese/MorraCineseProperties.asm
@@ -15,7 +15,7 @@ import ../../STDL/StandardLibrary
 import ../../STDL/CTLLibrary
 signature:
 	enum domain Sign = {CARTA | FORBICE | SASSO}
-	enum domain Result = {WINFIRST | WINSECOND | PATTA}
+	enum domain Result = {WINUSER | WINCOMPUTER | PATTA}
 	dynamic monitored userChoice: Sign //scelta dell'utente
 	dynamic monitored computerChoice: Sign //scelta del computer
 	dynamic controlled winner: Result
@@ -23,32 +23,31 @@ signature:
 definitions:
 
 	
-// reachability
-// esistono partite vinte da WINFIRST
-CTLSPEC ef(winner = WINFIRST)
-// esistono partite vinte da WINSECOND
-CTLSPEC ef(winner = WINSECOND)
-// esistono partite PATTA
+// reachability: esistono partite vinte da uno dei due giocatori o patta
+CTLSPEC ef(winner = WINUSER)
+CTLSPEC ef(winner = WINCOMPUTER)
 CTLSPEC ef(winner = PATTA)
 
 // liveness: e' sempre possibile che uno dei due giocatori vinca o che ci sia una patta
-CTLSPEC ag(ef(winner = WINFIRST))
-CTLSPEC ag(ef(winner = WINSECOND))
+CTLSPEC ag(ef(winner = WINUSER))
+CTLSPEC ag(ef(winner = WINCOMPUTER))
 CTLSPEC ag(ef(winner = PATTA))
+
+// e' patta se e solo se le due giocate sono uguali
+CTLSPEC    ag((userChoice = computerChoice) implies ax(winner=PATTA))
+// vince WINCOMPUTER se (computerChoice=FORBICE and userChoice=CARTA) or (computerChoice=CARTA and userChoice=SASSO) or (computerChoice=SASSO and userChoice=FORBICE)
+CTLSPEC  ag(((computerChoice = CARTA and userChoice = SASSO) or
+               (computerChoice = FORBICE and userChoice = CARTA) or
+               (computerChoice = SASSO and userChoice = FORBICE) ) implies ax(winner=WINCOMPUTER))
+// vince WINUSER se (userChoice=FORBICE and computerChoice=CARTA) or (userChoice=CARTA and computerChoice=SASSO) or (userChoice=SASSO and computerChoice=FORBICE)
+CTLSPEC  ag(((userChoice = CARTA and computerChoice = SASSO) or
+               (userChoice = FORBICE and computerChoice = CARTA) or
+               (userChoice = SASSO and computerChoice = FORBICE) ) implies ax(winner=WINUSER))
+
+//La partita è sempre patta
+CTLSPEC eg(winner = PATTA)
+
 /*
-
-
--- verificano la correttezza del modello
--- e' patta se e solo se le due giocate sono uguali; i giocatori A e B vincono
--- solo sotto determinate condizioni
-CTLSPEC    AG((choicePlayerA = choicePlayerB) <-> (winner=PATTA));
-CTLSPEC    AG( (  (choicePlayerA = CARTA & choicePlayerB = SASSO) |
-               (choicePlayerA = FORBICE & choicePlayerB = CARTA) |
-               (choicePlayerA = SASSO & choicePlayerB = FORBICE) ) <-> (winner=PLAYER_A));
-CTLSPEC    AG( (  (choicePlayerB = CARTA & choicePlayerA = SASSO) |
-               (choicePlayerB = FORBICE & choicePlayerA = CARTA) |
-               (choicePlayerB = SASSO & choicePlayerA = FORBICE) ) <-> (winner=PLAYER_B));
-
 -- verifica dell'esistenza di path particolari
 CTLSPEC    EG(winner = PATTA); -- sempre patta
 CTLSPEC    EX(EG(winner = PLAYER_A)); -- vince sempre PLAYER_A (EX perche' nello stato iniziale e' PATTA)
@@ -59,9 +58,11 @@ CTLSPEC    EX(EG(winner = PLAYER_B)); -- vince sempre PLAYER_B (EX perche' nello
 					winner := PATTA
 		else
 			if ((computerChoice=FORBICE and userChoice=CARTA) or (computerChoice=CARTA and userChoice=SASSO) or (computerChoice=SASSO and userChoice=FORBICE)) then
-				winner := WINFIRST
+				winner := WINCOMPUTER
 			else
-				winner := WINSECOND
+				winner := WINUSER
 			 endif 
 		endif
+		
 default init s0:
+	function winner = PATTA
