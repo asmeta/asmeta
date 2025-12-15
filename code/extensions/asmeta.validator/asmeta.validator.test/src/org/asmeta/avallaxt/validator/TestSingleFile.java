10b629f03feda34f27316e85d435b1179c63b103
Nico Pellegrinelli <nico.pellegrinelli@unibg.it>
2025-12-15 11:47:15 +0100

    remove ! from the grammar

--- code/extensions/asmeta.validator/asmeta.validator.test/src/org/asmeta/avallaxt/validator/TestSingleFile.java
+++ code/extensions/asmeta.validator/asmeta.validator.test/src/org/asmeta/avallaxt/validator/TestSingleFile.java
@@ -373,7 +373,9 @@ public class TestSingleFile extends TestValidator {
 		test(ASM_EXAMPLES + "PillBox/Level0/pillbox_0_scenario1.avalla", false, false, true);		
 	}
 
-	@Test
+/*	
+ 	// Added to test ! in function IDs
+ 	@Test
 	public void testPopulation() throws Exception {
 		// using extends 
 //		String scenario = "scenariosforexamples/population/withextend.avalla";
@@ -382,7 +384,7 @@ public class TestSingleFile extends TestValidator {
 		ASMParser.setUpReadAsm(new File("scenariosforexamples/population/population__tempAsmetaV6005497458607192670.asm"));
 		
 	}
-
+*/
 	
 	@Test
 	public void testlampada() throws Exception {
