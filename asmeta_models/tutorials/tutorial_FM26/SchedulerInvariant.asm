2c42aba1e5db5e16ee3f85f22216925a1a6d36b3
Angelo Gargantini <angelo.gargantini@unibg.it>
2026-01-29 18:35:45 +0100

    Add SchedulerInvariant ASM model with invariants
    Introduces a job scheduler ASM model that randomly selects ready jobs to run, terminates running jobs based on a monitored variable, and sets the scheduler to idle if no jobs are ready. This version includes invariants and properties for verification.

---
+++ asmeta_models/tutorials/tutorial_FM26/SchedulerInvariant.asm
@@ -0,0 +1,42 @@
+/*
+ * A simple job scheduler that at each step randomly choose a ready job and make it running. 
+ * At each step, running job are terminated using a controlled variable.
+ * If not job is ready, the scheduler is set to idle.
+ * 
+ * In this version we have added the invariants and the properties (not related to this tutorial)
+ */
+asm SchedulerInvariant
+
+import ../../STDL/StandardLibrary
+
+signature:
+	enum domain Job = {JOB1|JOB2|JOB3}
+	enum domain Status = {RDY|RUN|FIN}
+	controlled st: Job -> Status
+	controlled idle: Boolean
+	monitored fin: Job -> Boolean
+definitions:
+
+	macro rule r_SetRunning = 
+		choose $j1 in Job with st($j1) = RDY do 
+			st($j1) := RUN
+		ifnone 
+			idle:= true
+			
+	macro rule r_SetFinished = 
+		forall $j2 in Job with st($j2) = RUN do 
+			if fin($j2) then
+				st($j2) := FIN 
+			endif
+			
+	invariant inv_fin over Status : (forall $j in Job with fin($j) implies st($j) = RUN)		
+	
+	main rule r_Main =
+		par 
+			r_SetRunning[] 
+			r_SetFinished[] 
+		endpar
+		
+default init s0:
+	function idle = false
+	function st($j in Job) = RDY
