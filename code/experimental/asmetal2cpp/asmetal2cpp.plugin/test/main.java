e3ee9057cc1d5c3624968c797ebb454f440d8545
Angelo Gargantini <angelo.gargantini@unibg.it>
2026-01-30 23:37:22 +0100

    Refine tutorial checks and trim test imports
    Update S1_InvViolation.avalla to replace the generic "idle = false" check with explicit checks for all jobs being RDY, then that one is RUN, and finally mark all jobs finished. Also clean up code/experimental/asmetal2cpp/asmetal2cpp.plugin/test/main.java by removing many unused Swing and Eclipse imports, leaving only the GeneratorHandler import to simplify the test stub.

--- code/experimental/asmetal2cpp/asmetal2cpp.plugin/test/main.java
+++ code/experimental/asmetal2cpp/asmetal2cpp.plugin/test/main.java
@@ -1,31 +1,3 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Desktop.Action;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ItemEvent;
-
-import javax.swing.AbstractAction;
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.InputMap;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-import javax.swing.JComponent;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JRootPane;
-import javax.swing.KeyStroke;
-
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-
 import asm2code_inoproject.handlers.GeneratorHandler;
 
 public class main {
