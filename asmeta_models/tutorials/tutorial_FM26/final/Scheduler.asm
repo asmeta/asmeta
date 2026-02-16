93d3481e029403db9069b7fba657ca3c46471e6e
Nico Pellegrinelli <nico.pellegrinelli@unibg.it>
2026-02-16 10:00:01 +0100

    Add final version of FM26 scheduler tutorial scenarios and models

---
+++ asmeta_models/tutorials/tutorial_FM26/final/Scheduler.asm
@@ -0,0 +1,42 @@
+/*
+ * A simple job scheduler that at each step randomly choose a ready job and make it running. 
+ * At each step, running job may be terminated by the environment.
+ * If not job is ready, the scheduler is set to idle.
+ * 
+ */
+asm Scheduler
+import ../../../STDL/StandardLibrary
+
+signature:
+	// Domain definition
+	enum domain Job = {JOB1 | JOB2 | JOB3}
+	enum domain Status = {RDY | RUN | FIN}
+	// Controlled and out functions
+	dynamic controlled st: Job -> Status
+	dynamic out idle: Boolean
+	// Monitored functions
+	dynamic monitored fin: Job -> Boolean 
+	
+definitions:
+
+	macro rule r_SetRunning =
+		choose $j in Job with st($j) = RDY do
+			st($j) := RUN
+		ifnone
+			idle := true
+			
+	macro rule r_SetFinished =
+		forall $j in Job with st($j) = RUN do
+			if fin($j) then
+				st($j) := FIN
+			endif
+			
+	main rule r_Main =
+		par
+			r_SetRunning[]
+			r_SetFinished[]
+		endpar
+	
+default init s0:
+	function idle = false
+	function st($j in Job) = RDY
