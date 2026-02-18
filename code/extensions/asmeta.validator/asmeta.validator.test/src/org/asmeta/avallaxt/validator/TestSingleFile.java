a6339a01e62d06d21123cefda1ddae20e2ef4c02
Andrea Bombarda <andrea.bombarda@unibg.it>
2026-02-18 15:25:43 +0100

    deleted old tests

--- code/extensions/asmeta.validator/asmeta.validator.test/src/org/asmeta/avallaxt/validator/TestSingleFile.java
+++ code/extensions/asmeta.validator/asmeta.validator.test/src/org/asmeta/avallaxt/validator/TestSingleFile.java
@@ -449,10 +449,17 @@ public class TestSingleFile extends TestValidator {
 		String scenarioPath = ASM_EXAMPLES + "/../asmeta_models\\ABZ2026_CaseStudy\\SecondGeneration_AfterSimulation\\abstractests\\testtest0.avalla";
 		test(scenarioPath, false, false, true);		
 	}
+	
+	// 
+	@Test
+	public void testABZ26CaseStudyChatGPT() throws Exception {
+		String scenarioPath = ASM_EXAMPLES + "/../asmeta_models/ABZ2026_CaseStudy/SecondGeneration_AfterSimulation/scenarios_gpt/scenario0_fixed.avalla";
+		test(scenarioPath, false, false, true);		
+	}
 
 	@Test
 	public void testABZ26CaseStudyFolder() throws Exception {
-		String scenarioPath = ASM_EXAMPLES + "/../asmeta_models\\ABZ2026_CaseStudy\\SecondGeneration_AfterSimulation\\abstractests\\";
+		String scenarioPath = ASM_EXAMPLES + "/../asmeta_models/ABZ2026_CaseStudy/SecondGeneration_AfterSimulation/abstracttests/";
 		test(scenarioPath, true, true, true);		
 	}
 
