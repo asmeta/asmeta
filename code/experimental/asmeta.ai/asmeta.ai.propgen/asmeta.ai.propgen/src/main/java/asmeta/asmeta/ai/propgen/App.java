36100178c886447e6be9626b1140f457a096ab37
Angelo Gargantini <angelo.gargantini@unibg.it>
2026-03-31 10:14:43 +0200

    empty prooject for asmeta.ai.propgen

---
+++ code/experimental/asmeta.ai/asmeta.ai.propgen/asmeta.ai.propgen/src/main/java/asmeta/asmeta/ai/propgen/App.java
@@ -0,0 +1,13 @@
+package asmeta.asmeta.ai.propgen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
